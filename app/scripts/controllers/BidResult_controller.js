d2d0a7625eaba7ab70d71c084a5bca7c8daee440
firstjie333 <827348886@qq.com>
2014-08-13 01:39:07 +0800

    第四张卡：有序标签出错；先解决的价格排序的问题

--- app/scripts/controllers/BidResult_controller.js
+++ app/scripts/controllers/BidResult_controller.js
@@ -8,9 +8,25 @@ angular.module('angularApp')
     .controller('BidResultController', function ($scope,$location) {
 
 
-         $scope.bid_id=getLocal('details_bid').bid_id;
-         $scope.bid_messages_count=getLocal('page_bid_messages')==null ? 0 : getLocal('page_bid_messages').length;
+        $scope.bid_id=getLocal('details_bid').bid_id;
+        $scope.bid_messages_count=getLocal('page_bid_messages')==null ? 0 : getLocal('page_bid_messages').length;
 
+
+//排序：
+        //验证竞价是否是数字
+       // sortBy以升序排列
+        function sortByBidMessages()
+        {
+            var page_bid_messages=getLocal('page_bid_messages');
+             _.sortBy(page_bid_messages,function(page_bid_message)
+            {
+                return page_bid_message.user_price;
+            });
+        }
+        //显示
+
+
+        //结果：竞价为一人的里面的最低的那个竞价
 
 
 
